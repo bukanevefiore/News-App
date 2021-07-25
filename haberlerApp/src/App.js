4dbd0bc9f07d7efb979404308421297807afaaac
Melis Nur KILIÇ <fiorebucaneve22@gmail.com>
2021-07-26 00:56:16 +0300

    end

--- haberlerApp/src/App.js
+++ haberlerApp/src/App.js
@@ -6,39 +6,59 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import { View, Text, SafeAreaView, FlatList, StyleSheet } from 'react-native';
-import news_data from './news_data.json';
-import NewsCard from './components/NewsCard/NewsCard';
-
-function App() {
-  /* <FlatList
-          data={[1,2,3,4,5]}
-          renderItem={data => <Text>{data.item}</Text>}/>  */
-
-          const renderItem = ({item}) => <NewsCard news={item}/>;
-
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-
-        <FlatList
-          data={news_data}
-          renderItem={renderItem}
-          keyExtractor={(item ,index) => item.u_id.toString}
-        />
-      </View>
-    </SafeAreaView>
-  )
-}
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: '#eceff1',
-  
-  }
-})
-
-export default App;
+ import React from 'react';
+ import {
+   SafeAreaView,
+   Text,
+   Image,
+   StyleSheet,
+   FlatList,
+   ScrollView,
+   Dimensions,
+ } from 'react-native';
+ import NewsCard from './components/NewsCard';
+ 
+ import news_data from './news_data.json';
+ import news_banner_data from './news_banner_data.json';
+ 
+ function App() {
+   const renderNews = ({item}) => <NewsCard news={item} />;
+ 
+   return (
+     <SafeAreaView style={styles.container}>
+       <Text style={styles.headerText}>News</Text>
+       <FlatList
+         ListHeaderComponent={() => (
+           <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+             {news_banner_data.map(bannerNews => (
+               <Image
+                 style={styles.banner_image}
+                 source={{uri: bannerNews.imageUrl}}
+               />
+             ))}
+           </ScrollView>
+         )}
+         keyExtractor={item => item.u_id.toString()}
+         data={news_data}
+         renderItem={renderNews}
+       />
+     </SafeAreaView>
+   );
+ }
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#eceff1',
+   },
+   banner_image: {
+     height: Dimensions.get('window').height / 5,
+     width: Dimensions.get('window').width,
+   },
+   headerText: {
+     fontWeight: 'bold',
+     fontSize: 50,
+   },
+ });
+ 
+ export default App;
